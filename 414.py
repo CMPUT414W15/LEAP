d8d0d162f3abaa75d8dd531178577d10a9e5ec09
Infinity483 <aktsee@gmail.com>
2015-04-13 22:14:20 -0600

    updated script, created bvh generator

--- 414.py
+++ 414.py
@@ -3,10 +3,10 @@ from numpy import array, zeros, argmin, inf
 from numpy.linalg import norm
 from mathutils import *
 
-
 ####==========================================================================
 #   DTW implementation courtesy of Pierre Rouanet:
 #   http://github.com/pierre-rouanet/dtw
+#   See examples that he posts
 ####==========================================================================
 
 def dtw(x, y, dist=lambda x, y: norm(x - y, ord=1)):
@@ -70,7 +70,11 @@ def _trackeback(D):
 
 ####==========================================================================
 
-# Get the rotation given mocap, bonename, and frame
+####==========================================================================
+#   Get the rotation given mocap to search in, bonename, and frame
+#   "action" is a blender Action, "bonename" is a string, "frame" is an int
+#   blender Actions can be found in bpy.data.actions
+####==========================================================================
 def get_rotation(action, bonename, frame=1):
     rot = Euler([0,0,0])
     data_path = 'pose.bones["%s"].rotation_euler'%(bonename)
@@ -79,23 +83,24 @@ def get_rotation(action, bonename, frame=1):
             rot[fc.array_index] = fc.evaluate(frame)
     return(rot)
 
-# Creates a list containing the rotations of a bone
-# rot[i] equals the i+1th frame of animation
+####==========================================================================
+#   Creates a list containing the rotations of a bone
+#   rot[i] equals the i+1th frame of animation
+#   "action" is a blender Action, "bonename" is a string
+####==========================================================================
 def listRotation(action, bonename):
 	rot = []
 	for i in range(1, int(action.fcurves[0].range()[1]) + 1):
 		rot.append(get_rotation(action, bonename, i))
 	return rot
 
-# Replaces the existing rotation FCurves with the new one
+####==========================================================================
+#   Replaces the existing rotation FCurves with the new computed rotations
+#   "action" is a blender Action, "bonename" is a string and 
+#   "rot" is a list of vectors
+####==========================================================================
 def replaceRotation(action, bonename, rot):
     data_path = 'pose.bones["%s"].rotation_euler'%(bonename)
-    # c2 = action.fcurves.new(data_path)
-    # c2.array_index = 2
-    # c1 = action.fcurves.new(data_path)
-    # c1.array_index = 1
-    # c0 = action.fcurves.new(data_path)
-    # c0.array_index = 1
     x = []
     y = []
     z = []  
@@ -126,24 +131,49 @@ def replaceRotation(action, bonename, rot):
         c1k.insert(i, y[i-1], {'REPLACE'})
         c2k.insert(i, z[i-1], {'REPLACE'})
 
+####==========================================================================
+#   Creates the final curve based on determined path
+#   Based on current undrestanding of the function
+#   "curve" is a list of vectors and "path" is a list of ints
+####==========================================================================
+def match(curve, path):
+    t = []
+    for i in path:
+        t.append(curve[i])
+    return t
+
+####==========================================================================
+#   Run DTW alg to find shortest path
+#   Primarily interested in Path for the time being
+#   Uses it to generate the new rotations
+#   "curveA", "curveB" are a list of vectors
+####==========================================================================
+def applyDTW(curveA, curveB):
+    dist, cost, path = dtw(curveA, curveB)
+    curveA = match(curveA, path[0])
+    curveB = match(curveB, path[1])
+    return curveA, curveB
+
+####==========================================================================
+#   Example Usage
+####==========================================================================
 if __name__ == "__main__":
-	action = bpy.data.actions[0]
-	rotA = listRotation(action, 'lHand')
-	rotB = listRotation(action, 'rHand')
+	action1 = bpy.data.actions[0] # Mocap 1
+    action2 = bpy.data.actions[1] # Mocap 2
+    
+    # Comparison joints
+    bodyBone = 'lHand'
+    handBone = 'Hand'
 
-	dist, cost, path = dtw(rotA, rotB)
+    # Get the rotation data (vectors)
+	rotA = listRotation(action1, bodyBone)
+	rotB = listRotation(action2, handBone)
 
-	print("dist", dist)
-	print("cost", cost)
-	print("path", path)
+    # Process rotA and rotB
+    rotA, rotB = applyDTW(rotA, rotB)
+
+    # Replace originals
+    replaceRotation(action1, bodyBone, rotA)
+    replaceRotation(action2, handBone, rotB)
 
 
-
-
-
-
-
-fcurves = bpy.context.scene.node_tree.animation_data.action.fcurves
-
-for fcu in fcurves:
-    fcurves.remove(fcu)
