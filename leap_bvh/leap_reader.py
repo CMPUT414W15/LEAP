f67ec43c208bf87445c57358ecbce304c28fcf4f
Shen Wei Liao <shenweiliao@gmail.com>
2015-04-15 17:35:44 -0600

    Formatting.

--- leap_bvh/leap_reader.py
+++ leap_bvh/leap_reader.py
@@ -6,6 +6,7 @@
 # between Leap Motion and you, your company or other organization.            #
 ##########################################################################
 
+from __future__ import print_function
 import Leap
 import sys
 import numpy as np
@@ -13,6 +14,7 @@ from bvh import createHeader, createMotion
 
 
 class BVHListener(Leap.Listener):
+
     """ A LEAP listener that writes BVH """
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
@@ -38,86 +40,31 @@ class BVHListener(Leap.Listener):
         print(createMotion(self.channel_data, sum(self.frame_times)))
         # pass
 
+    def vec_to_str(self, v):
+        return " ".join(str(i) for i in [v.x, v.y, v.z])
+
+    def npmat(self, mat):
+        return np.matrix(np.reshape(np.array(mat), (3, 3)))
+
+    def mat_to_euler(self, _matrix):
+        from math import acos, atan2, pi
+        yaw = acos(_matrix[2, 2])
+        pitch = -atan2(_matrix[2, 0], _matrix[2, 1])
+        roll = -atan2(_matrix[0, 2], _matrix[1, 2])
+        # return yaw * (180 / pi), pitch * (180 / pi), roll * (180 / pi)
+        return yaw * (180 / pi), roll * (180 / pi), pitch * (180 / pi)
+
+    def leap_hand_eulers(self, hand):
+        normal = hand.palm_normal
+        direction = hand.direction
+        pitch = direction.pitch * Leap.RAD_TO_DEG
+        roll = normal.roll * Leap.RAD_TO_DEG
+        yaw = direction.yaw * Leap.RAD_TO_DEG
+        result = " ".join(str(i) for i in [yaw, roll, pitch])
+        return result
+
     def on_frame(self, controller):
-        """
-        # Get the most recent frame and report some basic information
         frame = controller.frame()
-
-        #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d,
-        gestures: %d" % (
-        #      frame.id, frame.timestamp, len(frame.hands), len(frame.fingers),
-        #      len(frame.tools), len(frame.gestures()))
-
-        # Get hands
-        for hand in frame.hands:
-
-            handType = "Left hand" if hand.is_left else "Right hand"
-
-            #print "  %s, id %d, position: %s" % (
-            #    handType, hand.id, hand.palm_position)
-
-            # Get the hand's normal vector and direction
-            normal = hand.palm_normal
-            direction = hand.direction
-
-            # Calculate the hand's pitch, roll, and yaw angles
-            #print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-            #    direction.pitch * Leap.RAD_TO_DEG,
-            #    normal.roll * Leap.RAD_TO_DEG,
-            #    direction.yaw * Leap.RAD_TO_DEG)
-
-            # Get arm bone
-            arm = hand.arm
-            #print "  Arm direction: %s, wrist position: %s, elbow position:
-            %s" % (
-            #    arm.direction,
-            #    arm.wrist_position,
-            #    arm.elbow_position)
-
-            # Get fingers
-            for finger in hand.fingers:
-
-                #print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
-                #    self.finger_names[finger.type()],
-                #    finger.id,
-                ##    finger.length,
-                #    finger.width)
-
-                # Get bones
-            for b in range(0, 4):
-                bone = finger.bone(b)
-                #    print "      Bone: %s, start: %s, end: %s, direction: %s,
-                #    basis: %s" % ( self.bone_names[bone.type],
-                #        bone.prev_joint,
-                #        bone.next_joint,
-                #        bone.direction,
-                #        bone.basis)
-        """
-        frame = controller.frame()
-
-        def vec_to_str(v):
-            return " ".join(str(i) for i in [v.x, v.y, v.z])
-
-        def npmat(mat):
-            return np.matrix(np.reshape(np.array(mat), (3, 3)))
-
-        def mat_to_euler(_matrix):
-            from math import acos, atan2, pi
-            yaw = acos(_matrix[2, 2])
-            pitch = -atan2(_matrix[2, 0], _matrix[2, 1])
-            roll = -atan2(_matrix[0, 2], _matrix[1, 2])
-            #return yaw * (180 / pi), pitch * (180 / pi), roll * (180 / pi)
-            return yaw * (180 / pi), roll * (180 / pi), pitch * (180 / pi)
-
-        def leap_hand_eulers(hand):
-            normal = hand.palm_normal
-            direction = hand.direction
-            pitch = direction.pitch * Leap.RAD_TO_DEG
-            roll = normal.roll * Leap.RAD_TO_DEG
-            yaw = direction.yaw * Leap.RAD_TO_DEG
-            result = " ".join(str(i) for i in [yaw, roll, pitch])
-            return result
-
         for hand in frame.hands:
             # do first frame things, i.e. HIERARCHY
             if self.first_frame == 0:
@@ -130,18 +77,19 @@ class BVHListener(Leap.Listener):
                 joints.insert(0, "Hand")
                 vector_offsets = [bone.next_joint - bone.prev_joint
                                   for bone in bones]
-                vector_offsets.insert(0, Leap.Vector(0,0,0))
-                offsets = [vec_to_str(v) for v in vector_offsets]
+                vector_offsets.insert(0, Leap.Vector(0, 0, 0))
+                offsets = [self.vec_to_str(v) for v in vector_offsets]
                 print(createHeader(joints, offsets))
                 self.first_frame = frame.id
 
             # do all frame things
             mat = hand.basis.rigid_inverse().to_array_3x3()
             hand_mat = np.matrix([[mat[0], mat[1], mat[2]],
-                                [mat[3], mat[4], mat[5]],
-                                [mat[6], mat[7], mat[8]]])
+                                  [mat[3], mat[4], mat[5]],
+                                  [mat[6], mat[7], mat[8]]])
 
-            frame_data = vec_to_str(hand.palm_position) + " " + leap_hand_eulers(hand) + " "
+            frame_data = self.vec_to_str(hand.palm_position) + " "
+            frame_data += self.leap_hand_eulers(hand) + " "
             for finger in hand.fingers:
                 finger_mat = hand_mat
                 for b in range(4):
@@ -150,14 +98,15 @@ class BVHListener(Leap.Listener):
                     # print(b, "b")
                     mat = bone.basis.rigid_inverse().to_array_3x3()
                     mat = np.matrix([[mat[0], mat[1], mat[2]],
-                                [mat[3], mat[4], mat[5]],
-                                [mat[6], mat[7], mat[8]]])
+                                     [mat[3], mat[4], mat[5]],
+                                     [mat[6], mat[7], mat[8]]])
                     finger_mat = np.linalg.inv(finger_mat) * mat
-                    yaw, roll, pitch = mat_to_euler(finger_mat)
-                    frame_data += "%s "%(yaw)
-                    frame_data += "%s "%(roll)
-                    frame_data += "%s "%(pitch)
-                    # frame_data += " ".join(str(i) for i in [yaw, roll, pitch]) + " "
+                    yaw, roll, pitch = self.mat_to_euler(finger_mat)
+                    print("yaw, pitch, roll", file=sys.stderr)
+                    print(yaw, pitch, roll, file=sys.stderr)
+                    frame_data += "%s " % (yaw)
+                    frame_data += "%s " % (roll)
+                    frame_data += "%s " % (pitch)
 
             self.channel_data.append(frame_data)
             self.frame_times.append(0.4)
