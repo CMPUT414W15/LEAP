7b6cc50344eab78169c220014f24b6f551f42ada
hwen3 <hwen3@ualberta.ca>
2015-04-20 01:18:27 -0600

    changed scaling into a class so that model names can be dynamically used, may still have issues

--- scaling.py
+++ scaling.py
@@ -2,107 +2,112 @@ import bpy
 import numpy
 import math
 
-# according to hypertextbook.com/facts/2006/bodyproportions.shtml
-# forarm+hand vs forarm ratio
-GOLDEN_RATIO = 1.618 
 
-# blender object modes
-EDIT_MODE = 'EDIT'
-OBJECT_MODE = 'OBJECT'
+class Scaler:
 
-# name of the hand objects after imported into blender
-HAND_LEFT = 'handLeft'
-HAND_RIGHT = 'handRight'
+    # according to hypertextbook.com/facts/2006/bodyproportions.shtml
+    # forarm+hand vs forarm ratio
+    GOLDEN_RATIO = 1.618
 
-# name of the forarm bone, should always be this name
-FOREARM = 'rForeArm'
+    # name of the forarm bone, should always be this name
+    FOREARM = 'rForeArm'
 
-# get the total length of all the children bone
-def childrenBoneLength(children):
-    length = 0
-    
-    for bone in children:
-        length += bone.length
-    
-    return length
+    # name of the hand objects after imported into blender
+    leftHandName = ''
+    rightHandName = ''
+    modelName = ''
 
-# calculate the length of the leap hand imported into blender
-def getLeapHandLength():
-    middleFinger = 'Middle_Metacarpal'
-    # assuming the model is always loaded after the body model
-    armatures = bpy.data.armatures[HAND_LEFT]
-    if armatures is not None:
-        bones = armatures.bones
-        if middleFinger in bones:
-            finger = bones[middleFinger]
-            return finger.length + childrenBoneLength(finger.children_recursive)
-    
-    return -1    
+    def __init__(self, modelName, leftHandName, rightHandName):
+        self.leftHandName = leftHandName
+        self.rightHandName = rightHandName
+        self.modelName = modelName
 
-# calculates the length of the hand on the model
-def getModelHandLength():
-    middleFinger = 'lMid1'
-    hand = 'lHand'
-    armatures = bpy.data.armatures[0]
-    if armatures is not None:
-        bones = armatures.bones
-        if middleFinger in bones:
-            finger = bones[middleFinger]
-            return bones[hand].length + finger.length + childrenBoneLength(finger.children_recursive)
-    
-    return -1   
+    # get the total length of all the children bone
+    def childrenBoneLength(self, children):
+        length = 0
+
+        for bone in children:
+            length += bone.length
+
+        return length
+
+    # calculate the length of the leap hand imported into blender
+    def getLeapHandLength(self):
+        middleFinger = 'Middle_Metacarpal'
+        # assuming the model is always loaded after the body model
+        armatures = bpy.data.armatures[self.leftHandName]
+        if armatures is not None:
+            bones = armatures.bones
+            if middleFinger in bones:
+                finger = bones[middleFinger]
+                return finger.length + self.childrenBoneLength(finger.children_recursive)
+
+        return -1
+
+    # calculates the length of the hand on the model
+    def getModelHandLength(self):
+        middleFinger = 'lMid1'
+        hand = 'lHand'
+        armatures = bpy.data.armatures[self.modelName]
+        if armatures is not None:
+            bones = armatures.bones
+            if middleFinger in bones:
+                finger = bones[middleFinger]
+                return bones[hand].length + finger.length + self.childrenBoneLength(finger.children_recursive)
+
+        return -1
+
+
+    # returns the length of the forearm of the model
+    def getForeArmLength(self):
+        # assuming the model is always loaded first
+        armatures = bpy.data.armatures[self.modelName]
+        if armatures is not None:
+            bones = armatures.bones
+            if self.FOREARM in bones:
+                return bones[self.FOREARM].length
+
+        return -1
+
+    # returns the scale factor required to scale the leap hand model
+    def getScaleFactor(self):
+        scaleFactor = 0
+
+        foreArmLength = self.getForeArmLength()
+        modelHandLength = self.getModelHandLength()
+        leapHandLength = self.getLeapHandLength()
+
+        if foreArmLength > 0:
+            if modelHandLength < 0:
+                # in case where there isnt a hand available
+                # we scale it based on the golden ratio
+                modelHandLength = (self.GOLDEN_RATIO*foreArmLength - foreArmLength)
+
+            scaleFactor = leapHandLength/modelHandLength
+
+        return math.fabs(scaleFactor)
+
+    # scales the hand
+    # possible other ways:
+    # 1. bpy.ops.transform.resize
+    # 2. iterate through every bone of the hand and resize
+    def scaleHand(self, hand):
+        bpySceneObj = bpy.context.scene.objects
+        bpyDataObj = bpy.data.objects
+
+        # set the active object to be resized
+        bpySceneObj.active = bpyDataObj.get(hand)
+        handModel = bpyDataObj[hand]
+        if handModel is not None:
+            scaleFactor = self.getScaleFactor()
+            if scaleFactor != 0:
+                scaleFactor = 1/scaleFactor
+                handModel.scale = (scaleFactor, scaleFactor, scaleFactor)
+
+        bpySceneObj.active = None
+
+    # scales both hands imported into leap
+    def scaleBothHands(self):
+        self.scaleHand(self.leftHandName)
+        self.scaleHand(self.rightHandName)
         
-
-# returns the length of the forearm of the model
-def getForeArmLength():
-    # assuming the model is always loaded first
-    armatures = bpy.data.armatures[0]
-    if armatures is not None:
-        bones = armatures.bones
-        if FOREARM in bones:
-            return bones[FOREARM].length
-    
-    return -1     
-
-# returns the scale factor required to scale the leap hand model
-def getScaleFactor():
-    scaleFactor = 0
-    
-    foreArmLength = getForeArmLength()
-    modelHandLength = getModelHandLength()
-    leapHandLength = getLeapHandLength()
-    
-    if foreArmLength > 0:
-        if modelHandLength < 0:
-            # in case where there isnt a hand available
-            # we scale it based on the golden ratio
-            modelHandLength = (GOLDEN_RATIO*foreArmLength - foreArmLength)
-            
-        scaleFactor = leapHandLength/modelHandLength
-    
-    return math.fabs(scaleFactor)
-
-# scales the hand
-# possible other ways:
-# 1. bpy.ops.transform.resize
-# 2. iterate through every bone of the hand and resize    
-def scaleHand(hand):
-    bpySceneObj = bpy.context.scene.objects
-    bpyOpsObj = bpy.ops.object
-    bpyDataObj = bpy.data.objects
-    
-    # set the active object to be resized
-    bpySceneObj.active = bpyDataObj.get(hand)
-    handModel = bpyDataObj[hand]
-    if handModel is not None:
-        scaleFactor = getScaleFactor()
-        if scaleFactor != 0:
-            scaleFactor = 1/scaleFactor
-            handModel.scale = (scaleFactor, scaleFactor, scaleFactor)        
-    
-    bpySceneObj.active = None 
-
-# scales both right and left hand
-def scaleBothHands():
-    scaleHand(HAND_LEFT)
-    scaleHandRight(HAND_RIGHT)
